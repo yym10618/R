44a5f6e70058d861108d3b786ca370243385a3dd
unknown <yym10618@gmail.com>
2022-04-18 15:43:48 +0900

    2022/04/18 R Ch01 수업예제

---
+++ Ch01/.Rproj.user/87D1BC8C/sources/s-EB34E6FD/5DBB3FD2-contents
@@ -0,0 +1,14 @@
+#날짜 : 2022/04/18
+#이름 : 양용민
+#내용 : Hello R
+
+print("Hello R!")
+print("안녕 R!")
+
+hello <- "Hello world"
+print(hello)
+
+welcome <- 'welcome R!'
+
+greeting <- 'Greeting R!'
+greeting
---
+++ Ch01/3_자료형.R
@@ -0,0 +1,29 @@
+#날짜 : 2022/04/18
+#이름 : 양용민
+#내용 : 자료형 실습하기 교재 p41
+
+#숫자형(정수, 실수)
+num1 <- 1
+num2 <- -2
+num3 <- 3.14
+num1;num2;num3
+
+#논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1
+bool2
+
+#문자형(문자, 문자열)
+str1 <- 'Hello'
+str2 <- '안녕'
+str1
+str2
+
+#특수형
+var1 <- NULL # 값이 없음을 의미하는 값, 초기값
+var2 <- NA   # 결측값, Not Avallable
+var3 <- NaN  # 수학적으로 정의가 불가능한 값, Not a Number
+var1
+var2
+var3
